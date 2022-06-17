33f86245eaa0c3ecfe28b8b6896c067bf6a0a8e7
Garrett Hays <GarrettlHays@gmail.com>
2022-06-17 09:11:56 -0700

    complete business logic and add some UI logic - WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,65 @@
 // business logic
-function Pizza() {
-  this.topping = topping;
+function Pizza(topping, size) {
+  this.topping = [];
   this.size = size;
-  this.price = this.pizzaPricer();
+  this.price = 0;
 }
 
 Pizza.prototype.pizzaPricer = function() {
-  const basePrice = 10;
-  let output = [basePrice, ""];
-  if (this.topping = 1 && this.size = small) {
-    output[0] += 2; 
+  if (this.topping = 1 && this.size === small) {
+    this.price += 12; 
   }
-  if (this.topping <= 2 && this.size = small) {
-    output[0] += 4;
+  if (this.topping <= 2 && this.size === small) {
+    this.price += 14;
   }
-  if (this.topping <= 4 && this.size = small) {
-    output[0] += 6;
+  if (this.topping <= 4 && this.size === small) {
+    this.price += 16;
   }
-  if (this.topping >= 4 && this.size = small) {
-    output[0] =+ 8;
+  if (this.topping >= 4 && this.size === small) {
+    this.price =+ 18;
   }
-  if (this.topping = 1 && this.size = medium) {
-    output[0] += 4; 
+  if (this.topping = 1 && this.size === medium) {
+    this.price += 14; 
   }
-  if (this.topping <= 2 && this.size = medium) {
-    output[0] += 6;
+  if (this.topping <= 2 && this.size === medium) {
+    this.price += 16;
   }
-  if (this.topping <= 4 && this.size = medium) {
-    output[0] += 8;
+  if (this.topping <= 4 && this.size === medium) {
+    this.price += 18;
   }
-  if (this.topping >= 4 && this.size = medium) {
-    output[0] =+ 10;
+  if (this.topping >= 4 && this.size === medium) {
+    this.price =+ 20;
   }
-  if (this.topping = 1 && this.size = large) {
-    output[0] += 6; 
+  if (this.topping = 1 && this.size === large) {
+    this.price += 16; 
   }
-  if (this.topping <= 2 && this.size = large) {
-    output[0] += 8;
+  if (this.topping <= 2 && this.size === large) {
+    this.price += 18;
   }
-  if (this.topping <= 4 && this.size = large) {
-    output[0] += 10;
+  if (this.topping <= 4 && this.size === large) {
+    this.price += 20;
   }
-  if (this.topping >= 4 && this.size = large) {
-    output[0] =+ 12;
+  if (this.topping >= 4 && this.size === large) {
+    this.price =+ 22;
   }
-  return output;
+  return this.price;
 };
 
-
 // UI logic
+$(document).ready(function() {
+  $("form#pizza").submit(function(event) {
+    event.preventDefault();
+  });
+});
+
+let pizzaSize = $("#size").val();
+let newPizza = new Pizza(topping,size);
+
+$("input[name='topping']:checked").each(function() {
+  newPizza.topping.push($(this).val());
+});
+
+newPizza.pizzaPricer();
+
+
+
