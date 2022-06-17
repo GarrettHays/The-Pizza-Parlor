9cdfbc4f7995a160eda41d2e36af88afd7aa3545
Garrett Hays <GarrettlHays@gmail.com>
2022-06-17 11:53:36 -0700

    update business logic to allow for more pizza sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,57 @@
 // business logic
-function Pizza(topping, size) {
+function Pizza(size) {
   this.topping = [];
   this.size = size;
   this.price = 0;
 }
 
 Pizza.prototype.pizzaPricer = function() {
-  if (this.topping = 1 && this.size === small) {
+  if (this.topping.length <= 1 && this.size === "small") {
     this.price += 12; 
-  }
-  if (this.topping <= 2 && this.size === small) {
+    console.log(this.topping.length);
+    console.log(this.size);
+  } else if (this.topping.length <= 2 && this.size === "small") {
     this.price += 14;
-  }
-  if (this.topping <= 4 && this.size === small) {
+  } else if (this.topping.length <= 4 && this.size === "small") {
     this.price += 16;
+  } else if (this.topping.length >= 4 && this.size === "small") {
+    this.price =+ 21;
   }
-  if (this.topping >= 4 && this.size === small) {
-    this.price =+ 18;
-  }
-  if (this.topping = 1 && this.size === medium) {
+  if (this.topping.length <= 1 && this.size === "medium") {
     this.price += 14; 
-  }
-  if (this.topping <= 2 && this.size === medium) {
+  } else if (this.topping.length <= 2 && this.size === "medium") {
     this.price += 16;
-  }
-  if (this.topping <= 4 && this.size === medium) {
+  } else if (this.topping.length <= 4 && this.size === "medium") {
     this.price += 18;
+  } else if (this.topping.length >= 4 && this.size === "medium") {
+    this.price =+ 23;
   }
-  if (this.topping >= 4 && this.size === medium) {
-    this.price =+ 20;
-  }
-  if (this.topping = 1 && this.size === large) {
+  if (this.topping.length <= 1 && this.size === "large") {
     this.price += 16; 
-  }
-  if (this.topping <= 2 && this.size === large) {
+  } else if (this.topping.length <= 2 && this.size === "large") {
     this.price += 18;
-  }
-  if (this.topping <= 4 && this.size === large) {
+  } else if (this.topping.length <= 4 && this.size === "large") {
     this.price += 20;
+  } else if (this.topping.length >= 4 && this.size === "large") {
+    this.price =+ 25;
   }
-  if (this.topping >= 4 && this.size === large) {
-    this.price =+ 22;
+  if (this.topping.length <= 1 && this.size === "Xlarge") {
+    this.price += 18; 
+  } else if (this.topping.length <= 2 && this.size === "Xlarge") {
+    this.price += 20;
+  } else if (this.topping.length <= 4 && this.size === "Xlarge") {
+    this.price += 22;
+  } else if (this.topping.length >= 4 && this.size === "Xlarge") {
+    this.price =+ 27;
+  }
+  if (this.topping.length <= 1 && this.size === "Giant") {
+    this.price += 18; 
+  } else if (this.topping.length <= 2 && this.size === "Giant") {
+    this.price += 20;
+  } else if (this.topping.length <= 4 && this.size === "Giant") {
+    this.price += 22;
+  } else if (this.topping.length >= 4 && this.size === "Giant") {
+    this.price =+ 27;
   }
   return this.price;
 };
@@ -49,17 +60,25 @@ Pizza.prototype.pizzaPricer = function() {
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
+
+    let pizzaSize = $("select#size").val();
+    let newPizza = new Pizza(pizzaSize);
+
+    $("input[name='topping']:checked").each(function() {
+      newPizza.topping.push($(this).val());
+    });
+
+    let pizzaPrice = newPizza.pizzaPricer();
+
+    $(".pizza-size-select").text(pizzaSize);
+    $(".pizza-topping-select").text(newPizza.topping);
+    $(".order-total").text(pizzaPrice);
+
+    // $("#pizza-order").show();
+    // $("#pizza-form").hide();
   });
 });
 
-let pizzaSize = $("#size").val();
-let newPizza = new Pizza(topping,size);
-
-$("input[name='topping']:checked").each(function() {
-  newPizza.topping.push($(this).val());
-});
-
-newPizza.pizzaPricer();
 
 
 
